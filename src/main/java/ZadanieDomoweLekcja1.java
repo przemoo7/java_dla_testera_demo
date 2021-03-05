c45d2ef9a9ff0caed2cee648e5bfc3ad28324ceb
Przemek <przemek.decsoft@gmail.com>
2021-03-05 18:10:08 +0100

    zadanie domowe 1 poprawki

--- src/main/java/ZadanieDomoweLekcja1.java
+++ src/main/java/ZadanieDomoweLekcja1.java
@@ -4,16 +4,19 @@ public class ZadanieDomoweLekcja1 {
 
         int numberOne = 1;
         int numberTwo = 2;
-        int numberThree = 3;
+        int numberThree = 4;
         int numberFour = 4;
 
         int sumA = numberOne + numberTwo;
         int sumB = numberThree + numberFour;
 
+        System.out.println("sumA");
+        System.out.println("sumB");
+
         if (sumA % 2 == 0 && sumB % 2 == 0) {
-            System.out.println("Liczby są parzyste");
+            System.out.println("Wszystkie liczby są parzyste");
         } else {
-            System.out.println("Liczby są nieparzyste");
+            System.out.println("Nie wszystkie liczby są parzyste");
         }
     }
 }

9b68e2bd0f95df633a50114535b8f5f537fa104b
Przemek <przemek.decsoft@gmail.com>
2021-03-05 15:14:01 +0100

    zmiany

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -1,6 +1,23 @@
 public class Demo {
 
     public static void main(String[] args) {
-        System.out.println("Demo");
+
+
+        byte b;
+        short s;
+        int i;
+        long l;
+
+        int number = 10;
+        System.out.println(number);
+
+        double doubleNumber = 10.5;
+        System.out.println(doubleNumber);
+
+        String someText = "text w javie";
+        System.out.println(someText);
+
+        char someChar = 's';
+        System.out.println(someChar);
     }
 }

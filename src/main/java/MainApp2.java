91cb63337e0d4fbfce557887c83a06e8c43aea09
Przemek <przemek.decsoft@gmail.com>
2021-03-05 17:53:15 +0100

    modulo

---
+++ src/main/java/MainApp2.java
@@ -0,0 +1,26 @@
+public class MainApp2 {
+
+    public static void main(String[] args) {
+
+
+//        nazwy klas
+//        User, SuperUser, AdminUser
+
+//        zmienne
+        String superUser = "jestem super user";
+        int number = 10;
+        boolean isTrue = true;
+
+        if (10 > 0) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+
+        }
+
+        System.out.println(10 % 5);
+        System.out.println(10 % 3);
+
+    }
+}
+

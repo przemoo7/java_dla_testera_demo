bfeaded698a4c5e32ae76ee58ee385cc05dbf492
Przemek <przemek.decsoft@gmail.com>
2021-03-07 16:22:33 +0100

    lekcja 4

--- src/main/java/MainApp3.java
+++ src/main/java/MainApp3.java
@@ -7,18 +7,28 @@ public class MainApp3 {
         User przemek = new User();
         przemek.firstName = "Przemek";
         przemek.lastName = "Testowy";
-        przemek.age = 30;
-        przemek.email ="przemek@test.pl";
-        przemek.isAdult = true;
-        System.out.println(przemek.firstName);
+        przemek.age = 10;
+        przemek.email = "przemek@test.pl";
+        przemek.isAdult = false;
+        przemek.getFullName();
+        przemek.getAllInfo();
+        System.out.println(przemek.getUserAge());
+        int userAge = przemek.getUserAge();
+        System.out.println(userAge);
+
+        boolean userAdult = przemek.isUserAdult();
+        System.out.println("Czy Przemek jest dorosły: " + userAdult);
 
         User tomek = new User();
         tomek.firstName = "Tomek";
         tomek.lastName = "Testowy";
-        tomek.age = 30;
-        tomek.email ="tomek@test.pl";
+        tomek.age = 20;
+        tomek.email = "tomek@test.pl";
         tomek.isAdult = true;
-        System.out.println(tomek.firstName);
-        System.out.println(tomek.isAdult);
+        tomek.getFullName();
+        tomek.getAllInfo();
+
+        boolean isTomekAdult = tomek.isUserAdult();
+        System.out.println("Czy Tomek jest dorosły: " + isTomekAdult );
     }
 }

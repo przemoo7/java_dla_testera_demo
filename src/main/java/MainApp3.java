45d37ab4dc96d24d45bce2862e9574e64f235405
Przemek <przemek.decsoft@gmail.com>
2021-03-07 18:36:33 +0100

    zadanie domowe 1

--- src/main/java/MainApp3.java
+++ src/main/java/MainApp3.java
@@ -1,43 +1,28 @@
 public class MainApp3 {
 
+//    public static void main(String[] args) {
+//
+////        Typ nazwa = nowy obiekt;
+//
+//        User przemek = new User("Przemek", "Testowy", "przemek@test.pl", 17);  // utworzenie obiektu poprzez wywowałanie konstruktora
+//        przemek.getAllInfo();
+//
+//        User tomek = new User("Tomek", "Testowy", "tomek@test.pl", 20);
+//        tomek.getAllInfo();
+//    }
+
     public static void main(String[] args) {
 
-//        Typ nazwa = nowy obiekt;
+        Bug bug1 = new Bug("Small bug", "email@test.pl", 2);
 
-        User przemek = new User();
-        przemek.firstName = "Przemek";
-        przemek.lastName = "Testowy";
-        przemek.age = 10;
-        przemek.email = "przemek@test.pl";
-        przemek.isAdult = false;
-        przemek.getFullName();
-        przemek.getAllInfo();
-        System.out.println(przemek.getUserAge());
-        int userAge = przemek.getUserAge();
-        System.out.println(userAge);
+        bug1.showBugStatus();
+        bug1.showAllBugInfo();
+        bug1.showEmail();
+        int bugPriority = bug1.getBugPriority();
+        System.out.println(bugPriority);
 
-        boolean userAdult = przemek.isUserAdult();
-        System.out.println("Czy Przemek jest dorosły: " + userAdult);
+        bug1.bugStatus = true;
 
-        przemek.greetings("Marcin");
-        przemek.greetings("Zuzia");
-        przemek.howOldAreYou("Przemek", 17);
-
-        int przemekAgePlus10 = przemek.yourAgePlus10(17);
-        System.out.println(przemekAgePlus10);
-
-        User tomek = new User();
-        tomek.firstName = "Tomek";
-        tomek.lastName = "Testowy";
-        tomek.age = 20;
-        tomek.email = "tomek@test.pl";
-        tomek.isAdult = true;
-        tomek.getFullName();
-        tomek.getAllInfo();
-
-        boolean isTomekAdult = tomek.isUserAdult();
-        System.out.println("Czy Tomek jest dorosły: " + isTomekAdult);
-
-        System.out.println(tomek.yourAgePlus10(tomek.age));
+        bug1.showBugStatus();
     }
 }

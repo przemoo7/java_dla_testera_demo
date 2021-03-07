740a7887dd8a9b864a34b28397269f50d63a8342
Przemek <przemek.decsoft@gmail.com>
2021-03-07 18:36:43 +0100

    zadanie domowe 2

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,4 +1,5 @@
 public class User {
+// KLASA - FORMA, SZABLON KTÓRA SLUŻY DO TWORZENIA OBIEKTÓW
 
 //   1. POLA OBIEKTU
 //    OPISUJĄ TO Z CZEGO BĘDZIE SKLADAL SIE OBIEKT
@@ -32,5 +33,38 @@ public class User {
         } else {
             return false;
         }
+    }
+
+    void greetings(String name) {
+        System.out.println("Hi " + name + ". Nice to see you!");
+    }
+
+    //    PRZECIĄŻANIE METOD - TWORZYMY METODY O TYCH SAMYCH NAZWACH, RÓŻNIĄCE SIĘ PARAMETRAMI (ILOścią, TYPAMI)
+    void greetings(String firstName, String lastName) {
+        System.out.println("Hi " + firstName + " " + lastName + ". Nice to see you!");
+    }
+
+
+    void howOldAreYou(String name, int userAge) {
+        System.out.println("Hi " + name + " you are " + userAge + " years old");
+    }
+
+    int yourAgePlus10(int userAge) {
+        return userAge + 10;
+    }
+
+    //    3. KONSTRUKTOR  - SLuZY do TWORZENIA OBIEKTOW
+//    DOMYSLNY KONSTRUKTOR - TWORZY PUSTY OBIEKT
+
+//    Nazwa konstruktora jest taka sama jak nazwa klasy
+//    Konstruktor piszemy wielka litera
+//    Konstruktor nie ma zwracanego typu
+
+    User(String firstName, String lastName, String email, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.age = age;
+        this.isAdult = isUserAdult();
     }
 }

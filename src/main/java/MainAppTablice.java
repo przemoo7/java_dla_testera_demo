3bfffd6564950ceddbbea113ee011016cc1c6d51
Przemek <przemek.decsoft@gmail.com>
2021-03-10 21:31:08 +0100

    tablice

---
+++ src/main/java/MainAppTablice.java
@@ -0,0 +1,16 @@
+public class MainAppTablice {
+
+    public static void main(String[] args) {
+
+//        1.  Tablica - zbiór danych tego samego typu
+//        2. Zawsze musimy okreslić rozmiar tablicy
+//        3. Ostatni element - długość tablicy
+//        4. [] - tablica
+//        5. int [] numbers = new int[10]
+
+        String[] names = new String[5];
+
+//        wartości tablicy ["Przemek", "Dżemek", "Zuzia", "Fiodor", "Bambuła"]
+//        index tablicy      [  0   ,      1   ,    2    ,   3   ,      4]
+    }
+}
